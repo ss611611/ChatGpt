f1b15dcba9a482f04e09bc78794f4f9f75a34b1f
ss611611 <ss611611@gmail.com>
2023-02-27 21:33:01 +0800

    Build Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,19 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html>
+
+      <head/>
+
+      <body>
+        <div className="flex">
+          {/* Sidebar */}
+
+          {/* ClientProvider - Notification */}
+          
+          <div className="bg-[#343541] flex-1">{children}</div>
+        </div>
+      </body>
     </html>
   )
 }
