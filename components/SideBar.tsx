8e4d8079b135195820ff4de793238cc23a9eea87
ss611611 <ss611611@gmail.com>
2023-03-01 20:22:32 +0800

    Building the Side Bar Component (4/5)

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,16 +1,22 @@
 "use client";
 
 import { useSession, signOut } from "next-auth/react";
+import { useCollection } from "react-firebase-hooks/firestore";
 import NewChat from "./NewChat";
-import { collection } from "firebase/firestore";
+import { collection, orderBy, query } from "firebase/firestore";
+import { db } from "../firebase";
+import ChatRow from "./ChatRow";
 
 function SideBar() {
   const { data: session } = useSession();
 
   const [chats, loading, error] = useCollection(
-    session && collection(db, "users", session.user?.email!, "chats")
+    session && 
+    query(
+      collection(db, "users", session.user?.email!, "chats"),
+      orderBy("createdAt", "asc")
+    )
   );
-
 
   return (
     <div className="p-2 flex flex-col h-screen">
@@ -21,7 +27,7 @@ function SideBar() {
           <div>{/* ModelSelection */}</div>
 
           {/* Map through the ChatRows */}
-          {chats?.docs.map(chat => (
+          {chats?.docs.map((chat) => (
             <ChatRow key={chat.id} id={chat.id} />
           ))}
         </div>
