18135b5378695ccad79c9232ded272277364163e
ss611611 <ss611611@gmail.com>
2023-03-08 18:50:16 +0800

    Setting up OpenAI API Key (3/3)

--- pages/api/askQuestion.ts
+++ pages/api/askQuestion.ts
@@ -1,16 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import query from '../../lib/queryApi';
+import admin from "firebase-admin";
+import { adminDb } from '../../firebaseAdmin';
 
 type Data = {
-  name: string
+  answer: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-    const { prompt, chatId, model, Session } = req.body;
+    const { prompt, chatId, model, session } = req.body;
 
     if (!prompt) {
         res.status(400).json({ answer: "Please provide a prompt!" });
@@ -27,7 +29,16 @@ export default function handler(
 
     const message: Message = {
         text: response || "ChatGPT was unable to find an answer fot that!",
-    }
+        createdAt: admin.firestore.Timestamp.now(),
+        user: {
+          _id:"ChatGPT",
+          name: "ChatGPT",
+          avatar: "https://links.papareact.com/89k",
+        },
+    };
 
-  res.status(200).json({ name: 'John Doe' })
+    await adminDb.collection("users").doc(session?.user?.email).collection("chats").doc(chatId).collection("messages").add(message);
+
+
+  res.status(200).json({ answer: message.text });
 }
