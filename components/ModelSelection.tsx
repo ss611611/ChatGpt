6c71d5b961d05e1b7da0c552e134fc9f91227297
ss611611 <ss611611@gmail.com>
2023-03-09 19:57:53 +0800

    Explaining & Implementing useSWR

--- components/ModelSelection.tsx
+++ components/ModelSelection.tsx
@@ -1,7 +1,11 @@
+"ues client";
+import useSWR from "swr";
+
+const fetchModels = () => fetch("/api/getEngines").then((res) => res.json());
+
 function ModelSelection() {
-  return (
-    <div>ModelSelection</div>
-  )
+  const { data: models, isLoading } = useSWR("models", fetchModels);
+  return <div>ModelSelection</div>;
 }
 
-export default ModelSelection
+export default ModelSelection;
