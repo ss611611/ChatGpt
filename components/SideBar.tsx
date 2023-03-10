6d1296ededd96e85a49a52ab01d9a4b38e20259a
ss611611 <ss611611@gmail.com>
2023-03-10 19:35:41 +0800

    Building the Side Bar Component (5/5)

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -29,10 +29,18 @@ function SideBar() {
             <ModelSelection />
           </div>
 
+
+          <div className="flex flex-col space-y-2 my-2">
+            {loading && (
+              <div className="animate-pulse text-center text-white">
+                <p>Loading Chats...</p>
+              </div> 
+            )}
           {/* Map through the ChatRows */}
           {chats?.docs.map((chat) => (
             <ChatRow key={chat.id} id={chat.id} />
           ))}
+          </div>
         </div>
       </div>
       
